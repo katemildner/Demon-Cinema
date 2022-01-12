a78970d38bc0e0f611e1e5bbd638d73bb528a291
Katarzyna Mildner <mildner.katarzyna@gmail.com>
2022-01-13 00:27:16 +0100

    Fix style and layout offences

--- app/controllers/cinema_halls_controller.rb
+++ app/controllers/cinema_halls_controller.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 class CinemaHallsController < ApplicationController
-  before_action :set_cinema_hall, only: %i[ show edit update destroy ]
+  before_action :set_cinema_hall, only: %i[show edit update destroy]
 
   def index
     @cinema_halls = CinemaHall.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @cinema_hall = CinemaHall.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @cinema_hall = CinemaHall.new(cinema_hall_params)
 
     respond_to do |format|
       if @cinema_hall.save
-        format.html { redirect_to cinema_hall_url(@cinema_hall), notice: "Cinema hall was successfully created." }
+        format.html { redirect_to cinema_hall_url(@cinema_hall), notice: 'Cinema hall was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -30,7 +30,7 @@ class CinemaHallsController < ApplicationController
   def update
     respond_to do |format|
       if @cinema_hall.update(cinema_hall_params)
-        format.html { redirect_to cinema_hall_url(@cinema_hall), notice: "Cinema hall was successfully updated." }
+        format.html { redirect_to cinema_hall_url(@cinema_hall), notice: 'Cinema hall was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -41,16 +41,17 @@ class CinemaHallsController < ApplicationController
     @cinema_hall.destroy
 
     respond_to do |format|
-      format.html { redirect_to cinema_halls_url, notice: "Cinema hall was successfully destroyed." }
+      format.html { redirect_to cinema_halls_url, notice: 'Cinema hall was successfully destroyed.' }
     end
   end
 
   private
-    def set_cinema_hall
-      @cinema_hall = CinemaHall.find(params[:id])
-    end
 
-    def cinema_hall_params
-      params.require(:cinema_hall).permit(:name, :number_of_rows, :number_of_columns)
-    end
+  def set_cinema_hall
+    @cinema_hall = CinemaHall.find(params[:id])
+  end
+
+  def cinema_hall_params
+    params.require(:cinema_hall).permit(:name, :number_of_rows, :number_of_columns)
+  end
 end
--- spec/factories/cinema_halls.rb
+++ spec/factories/cinema_halls.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :cinema_hall do
-    name { "MyString" }
+    name { 'MyString' }
     number_of_rows { 1 }
     number_of_columns { 1 }
   end
